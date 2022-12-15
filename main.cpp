220a1f667058539d71b6d8a11a98fb7a8d53b20b
niccolomozzatii <118431871+niccolomozzatii@users.noreply.github.com>
2022-12-15 16:14:37 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,5 +1,29 @@
 #include <iostream>
-
+#include "lib.h"
+using namespace std;
 int main(){
-  return 0;
+    char lista[10][20];
+    char nome[20];
+    for (int x = 0; x < 10; x++) {
+        for (int y = 0; y < 20; y++) {
+            lista[x][y] = ' ';
+        }
+    }
+    for (int x = 0; x < 20; x++) {
+        nome[x] = ' ';
+    }
+    for (int x = 0; x < 10; x++) {
+        cin >> lista[x];
+    }
+    for (int x = 0; x < 10; x++) {
+        cout << lista[x] << endl;
+    }
+    cin >> nome;
+    int x = posizione(lista, nome);
+    if (x < 10) {
+        cout << x;
+    } else if (x == 10) {
+        cout << "non presente";
+    }
+    return 0;
 }
