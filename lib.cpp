f0e649feadc538733152ad33a36aa51849a1f7fc
niccolomozzatii <118431871+niccolomozzatii@users.noreply.github.com>
2022-12-15 16:11:17 +0100

    Update lib.cpp

--- lib.cpp
+++ lib.cpp
@@ -1,1 +1,12 @@
-
+int posizione(char lista[10][20], char nome[20]){
+    int i = 0;
+    int j = 0;
+    while ((j < 19) and (i < 10)) {
+        if (lista[i][j] != nome[j]) {
+            i++;
+            j = 0;
+        } else if (lista[i][j] == nome[j]) {
+            j++;
+        }
+    } return i;
+}
